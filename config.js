384e0d045f69ef547e61b9bef019a2a22e8524b7
ManavvGarg <58691392+ManavvGarg@users.noreply.github.com>
2020-10-27 22:09:28 +0530

    UPDATE#2

--- config.js
+++ config.js
@@ -1,38 +1,23 @@
-exports.TOKEN = "NzUwMzIwODA4MTQ4MDc0NTA3.X040tA.uaSL6VyjqaPkCjMNZb7MRWu1qaM";
-exports.PREFIX = 'm/';
+exports.TOKEN = "YOUR BOT TOKEN";
 
-exports.GOOGLE_API_KEY = 'AIzaSyAHvM0ZU04cOsILBB0tTcGFykM3fXetmHU';
+exports.PREFIX = 'YOUR PREFIX';
 
-exports.GENIUS_API_KEY = 'zeLA8irJR0Or0ZhmCROAmUkf6m4-nl-cwwwpH28LwS86Mx09S8BLy-9tlPrsvOpk';
+exports.GOOGLE_API_KEY = 'YOUR GOOGLE API KEY';
 
-exports.news_API = '';
-
-exports.giphy_API = '';
-
-exports.AME_API = '951dd0489be2ae4e0ed23c45d1744a69fb64488a90db5a68caeab15b235d2744ff1b4972591a13ca7d46fc566656c936ab3355952757580e5595633b75176c8f';
-
-exports.ownerID = ["621764740573560842" ,"486133445307924497", "684092617272721420"]
+exports.GENIUS_API_KEY = 'YOUR GENIUS API KEY HERE FOR LYRICS';
 
 exports.MAX_PLAYLIST_SIZE = 200;
 
-exports.hostedBy = true;
-
 exports.LAVA_ID = "default"
 
-exports.LAVA_HOST = "lava.danbot.host"
+exports.LAVA_HOST = "IP ADDRESS HERE" //your lavalink node/server ip address or the site
 
-exports.LAVA_PORT = "2333"
+exports.LAVA_PORT = "PORT HERE" //port
 
-exports.LAVA_PASSWORD = "DBH"
+exports.LAVA_PASSWORD = "PASS HERE" //pass. if pass not available, use default "youshallnotpass"
 
-exports.OWNER_ID = "684092617272721420"
+exports.OWNER_ID = "YOUR ACCOUNT ID HERE" //your account id
 
-exports.STEAM = "7B900ED71BCAE95C8F6AE75F5F18C921"
+exports.spCID = "YOUR SPOTIFY CLIENT ID"
 
-exports.spCID = "8baaa509b53f4d999b8d751d74bd3f23"
-
-exports.spCS = "02d2b759e11240c2bfa73fe604c74e93"
-
-exports.allow_more_than_200_chars = "yes"
-
-exports.r6Key = "6bc89c9a-9e00-4c81-baec-7a3e7da5b003"
+exports.spCS = "your spotify client SECRET"
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 //====================================================================================CONSTANTS REQUIRED ON READY=============================================================================================
-const { Client, Collection, MessageEmbed, Structures, MessageAttachment } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 const { PREFIX, TOKEN } = require('./config');
 const bot = new Client({ disableMentions: 'everyone' });
 const fs = require("fs");
 const db = require('quick.db');
-const canvacord = require("canvacord")
 //============================================================================================================================================================================================================
 
 
 //====================================================================================COLLECTIONS REQUIRED ON READY===========================================================================================
 bot.commands = new Collection();
-bot.aliases = new Collection();
-bot.snipes = new Collection();
 
 //============================================================================================================================================================================================================
 
@@ -29,72 +26,7 @@ bot.categories = fs.readdirSync("./commands/");
 
 //============================================================================================================================================================================================================
 
-
-
-
-//========================================================================================GiveawayManagerModule==============================================================================================
-
-const { GiveawaysManager } = require('discord-giveaways');
-bot.giveawaysManager = new GiveawaysManager(bot, {
-    storage: "./giveaways.json",
-    updateCountdownEvery: 5000,
-    default: {
-        botsCanWin: false,
-        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
-        embedColor: "#FF0000",
-        reaction: "🎉"
-    }
-});
-
-//============================================================================================================================================================================================================
-
-
-
-//=============================================================================================MULTI SNIPE====================================================================================================
-
-bot.on("messageDelete", async (message) => {
-    try {
-      if (message.author.bot) return;
-      const snipes = message.client.snipes.get(message.channel.id) || [];
-      snipes.unshift({
-        content: message.content,
-        author: message.author,
-        image: message.attachments.first()
-          ? message.attachments.first().proxyURL
-          : null,
-        date: new Date().toLocaleString("en-GB", {
-          dataStyle: "full",
-          timeStyle: "short",
-        }),
-      });
-      snipes.splice(20);
-      message.client.snipes.set(message.channel.id, snipes);
-    } catch (e) { console.log(e) }
-
-    try {
-        
-      let embed = new MessageEmbed()
-      .setTitle(`New message deleted!`)
-      .setDescription(
-        `**The user \`${message.author.tag}\` has deleted a message in <#${message.channel.id}>**`
-      )
-      .addField(`Content:`, message.content, true)
-      .setColor(`RED`);
-    let channel = db.fetch(`modlog_${message.guild.id}`)
-    let ctx = bot.guilds.cache.get(message.guild.id).channels.cache.get(channel)
-    if (!ctx) return;
-    ctx.send(embed)
-
-    }
-    catch(e) { console.log(e) }
-    });
-
-//============================================================================================================================================================================================================
-
-
-
-
-//=========================================================================================AFK AND MENTION SETTINGS===========================================================================================
+//========================================================================================= MENTION SETTINGS ===========================================================================================
 
 bot.on('message', async message => {
 
@@ -120,56 +52,8 @@ bot.on('message', async message => {
         return;
     };
 
-//============================================================================================== AFK =========================================================================================================
-let afk = db.fetch(`afk_${message.guild.id}_${message.author.id}`)
- 
-
-if(afk && !message.content.startsWith(`${prefix}afk`)){
-   
- let mentions =  db.fetch(`mentions_${message.guild.id}_${message.author.id}`)
-if(!mentions) mentions  = 0;
-    message.channel.send(`You are no longer afk for [   ${afk}   ] \nWhile afk you got \`${mentions}\` ping(s)`)
-    
-    db.delete(`afk_${message.guild.id}_${message.author.id}`)
-    db.delete(`mentions_${message.guild.id}_${message.author.id}`)
- }
- if(message.mentions.users.first()){
-     let user = message.mentions.users.first();
-     let uafk = db.fetch(`afk_${message.guild.id}_${user.id}`)
-if(uafk){ db.add(`mentions_${message.guild.id}_${user.id}`,1)
-message.channel.send(`\`${user.username}\` is currently afk: ${uafk}`)
-
-}
- }
 });
 
 //============================================================================================================================================================================================================
-
-
-//================================================================================================ WELCOME ===================================================================================================
-bot.on("guildMemberAdd", (member) => {
-    let chx = db.get(`welchannel_${member.guild.id}`);
-    let msg = db.get(`welmsg_${member.guild.id}`);
-  
-    if(chx === null) {
-      return;
-    }
-  
-    let welcomeIMG = new canvacord.Welcomer(
-      {
-        username: member.user.username,
-        discrim: member.user.discriminator,
-        avatarURL: member.user.displayAvatarURL()
-      }
-    )
-    canvacord.write(image, "welcome.png");
-    let attachment = new MessageAttachment(image, "welcome.png");
-
-    bot.channels.cache.get(chx).send(attachment);
-  
-})
-
-//============================================================================================================================================================================================================
-
 
 bot.login(TOKEN);
